3ce768030b1a597153348b2c62d19d8fc5297cd7
tumobi <tumobi@163.com>
2018-10-27 18:08:19 +0800

    登录添加验证码功能

--- egg-server/app/controller/login.js
+++ egg-server/app/controller/login.js
@@ -1,5 +1,6 @@
 'use strict';
 const jwt = require('jsonwebtoken');
+const svgCaptcha = require('svg-captcha');
 const BaseController = require('./base');
 
 class LoginController extends BaseController {
@@ -20,6 +21,12 @@ class LoginController extends BaseController {
       this.error(400, '请求参数不合法');
       return;
     }
+    console.log(this.ctx.session.captcha, captcha);
+    // 判断验证码
+    if (!this.ctx.session.captcha || this.ctx.session.captcha.toLowerCase() !== captcha.toLowerCase()) {
+      this.error(400, '验证码错误');
+      return;
+    }
 
     if (username !== 'admin' || password !== 'nideadmin') {
       this.error(400, '用户名或密码错误');
@@ -30,6 +37,19 @@ class LoginController extends BaseController {
     const user = { id: 1, username: 'admin', avatar: '' };
     const token = jwt.sign({ user_id: 1 }, this.config.jwt.secret);
     this.success({ user, token });
+  }
+
+  async captcha() {
+    const captcha = svgCaptcha.create({
+      size: 4, // 字符数量
+      noise: 4, // 干扰线数量
+      color: true, // 彩色显示
+      ignoreChars: '0o1i', // 忽略的字符
+      background: '#f8f8f9', // 背景色
+    });
+    this.ctx.session.captcha = captcha.text;
+    this.ctx.set('Content-Type', 'image/svg+xml');
+    this.ctx.body = captcha.data;
   }
 }
 
--- egg-server/app/router.js
+++ egg-server/app/router.js
@@ -6,5 +6,6 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  router.get('/login/captcha', controller.login.captcha);
   router.post('/login', controller.login.login);
 };
